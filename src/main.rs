c46176c8156fa5938d43dc696ce8ad50a45047dd
Connor McNaboe <mcnaboeconnor@gmail.com>
2023-06-29 12:37:09 -0600

    2-integrate-tareldar: Refactor to allow cors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use actix_web::{middleware, App, HttpServer};
+use actix_web::{middleware, App, HttpServer, http};
 use navigator::app_config::config_app;
+use actix_cors::Cors;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -8,9 +9,17 @@ async fn main() -> std::io::Result<()> {
     log::info!("starting HTTP server at http://localhost:8080");
 
     HttpServer::new(|| {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
+
         App::new()
             .configure(config_app)
             .wrap(middleware::Logger::default())
+            .wrap(cors )
     })
         .bind(("127.0.0.1", 8080))?
         .run()
